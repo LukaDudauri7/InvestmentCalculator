0822d7c5bb64a4e23056d41f7804b1544bc1a9bf
LukaDudauri7 <lukadudauri01@gmail.com>
2024-04-08 10:49:47 +0400

    valid duration

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ function App() {
     duration: 10
   });
   
+  const inputIsValid = userInput.duration >= 1;
   function handleChange(inputIdentifier, newValue) {
     setUserInput(prevUserInput => {
         return{
@@ -24,7 +25,8 @@ function App() {
     <>
       <Header />
       <UserInput onChange={handleChange} userInput={userInput}/>
-      <Results input={userInput}/>
+      {!inputIsValid && <p className="center">Please enter a duration greater than zero.</p>}
+      {inputIsValid && <Results input={userInput}/>}
     </>
   )
 }
